400c63692b8ad9878b34c43ae0a6578b3adc40e2
Sdalv <sdalvens@gmail.com>
2022-06-19 23:00:59 -0400

    Git update

---
+++ June._py/Pythondatatype/datatype.py/datatype.py
@@ -0,0 +1,23 @@
+A=1
+B=2
+Result = A + B
+print(Result)
+
+A= "Sachin"
+print(A)
+
+A= 1
+B= 2
+Results = A/B
+print(Results)
+
+X = 12
+Y = 15
+sum = X + Y
+results = sum
+print(type(results))
+
+Savings = 100
+Multiplier = 1.1
+results= Savings * Multiplier ** 7
+print(results)
