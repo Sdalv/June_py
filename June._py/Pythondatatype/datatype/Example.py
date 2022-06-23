2a5d37d3ec44c5f05212ae4a400b5c9805818dfe
Sdalv <sdalvens@gmail.com>
2022-06-23 10:33:28 -0400

    Update

---
+++ June._py/Pythondatatype/datatype/Example.py
@@ -0,0 +1,21 @@
+x = 4
+print(x % 7)
+print(x % 7 +6)
+
+name = "Bob"
+surname= "Smith"
+print(name + surname)
+A= 6/8
+print(A)
+print(3/8)
+A= [[1,2,3], ["A", "B", "C"]]
+B= list(A)
+B[0]= 5
+print(A)
+print(B)
+a= "python * 2"
+print(type(a))
+b= "Python" +  "2"
+print(b)
+print(False * bool(3.2))
+print(type(int(6.5) + True + bool("python")))
