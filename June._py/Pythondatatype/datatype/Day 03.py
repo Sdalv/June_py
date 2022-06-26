8f62167c6f292d7a773f27ad0c0ed0ad4be82784
Sdalv <sdalvens@gmail.com>
2022-06-26 15:37:19 -0400

    Day 03

--- June._py/Pythondatatype/datatype/Day 03.py
+++ June._py/Pythondatatype/datatype/Day 03.py
@@ -1,5 +1,25 @@
 #Day 03
 A= ["Fabian", 6.00, "Sachin", 6.00, "Archana", 5.5]
 B= ["Phone", "222-222-2222", "Email", "fabian@company.com"]
-print(type(B))
+for Res in  A:
+	print(Res)
+for x in B:
+	print(x)
+
+
+#z = [-1.2,4,3.6,-5,1,-2,2.5]
+#print(max(z))
+
+print(type(B[1]))
+
+import math
+math.pi
+
+R= 0.4
+C= math.pi * 2 * R
+
+Area= math.pi * R** 2
+print("Circumference: " + str(C))
+print("Area: " + str(Area))
+
 
